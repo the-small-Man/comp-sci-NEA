1a487345b8c5f916b32f158c81997b05ce06b85d
the-small-Man <mss1907482@malmesbury.wilts.sch.uk>
2025-04-27 20:52:55 +0100

    Update movement_prototype.py

--- movement_prototype.py
+++ movement_prototype.py
@@ -2,30 +2,27 @@ import pygame as pg
 from star_prototype import star as st
 
 
-class mainGame():
 
-    def __init__(self):
-        pg.init()
+pg.init()
 
-        self.clock = pg.time.Clock()
+clock = pg.time.Clock()
 
-        self.screen=pg.display.set_mode((0,0), pg.RESIZABLE)
-        pg.display.set_caption("Shadows Chasing Stars")
-        self.width=0
-        self.height=0
-        self.isRunning = True
+screen=pg.display.set_mode((0,0), pg.RESIZABLE)
+pg.display.set_caption("Shadows Chasing Stars")
+width=0
+height=0
 
 
-
-while isRunning:
+while True:
     for ev in pg.event.get():
         if ev.type == pg.VIDEORESIZE:
             width = ev.w
             height = ev.h
         if ev.type == pg.QUIT:
-            isRunning = False
+            pg.quit()
         if ev.type == pg.KEYDOWN:
             if ev.key == pg.K_ESCAPE:
-                isRunning = False
+                pg.quit()
+                
     pg.display.update()
-    mainGame.clock.tick(60)
+    clock.tick(60)

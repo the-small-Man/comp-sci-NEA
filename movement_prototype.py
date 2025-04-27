1bfa3a7b295cf737f3b9901f6a2f5792440a26b6
the-small-Man <mss1907482@malmesbury.wilts.sch.uk>
2025-04-27 21:11:21 +0100

    i classified the running function

--- movement_prototype.py
+++ movement_prototype.py
@@ -2,27 +2,30 @@ import pygame as pg
 from star_prototype import star as st
 
 
+class mainGame:
+    def __init__(self):
+        pg.init()
 
-pg.init()
+        self.clock = pg.time.Clock()
 
-clock = pg.time.Clock()
+    screen=pg.display.set_mode((0,0), pg.RESIZABLE)
+    pg.display.set_caption("Shadows Chasing Stars")
+    width=0
+    height=0
 
-screen=pg.display.set_mode((0,0), pg.RESIZABLE)
-pg.display.set_caption("Shadows Chasing Stars")
-width=0
-height=0
+    def run(self):
+        while True:
+            for ev in pg.event.get():
+                if ev.type == pg.VIDEORESIZE:
+                    self.width = ev.w
+                    self.height = ev.h
+                if ev.type == pg.QUIT:
+                    pg.quit()
+                if ev.type == pg.KEYDOWN:
+                    if ev.key == pg.K_ESCAPE:
+                        pg.quit()
+                        
+            pg.display.update()
+            self.clock.tick(60)
 
-
-while True:
-    for ev in pg.event.get():
-        if ev.type == pg.VIDEORESIZE:
-            width = ev.w
-            height = ev.h
-        if ev.type == pg.QUIT:
-            pg.quit()
-        if ev.type == pg.KEYDOWN:
-            if ev.key == pg.K_ESCAPE:
-                pg.quit()
-                
-    pg.display.update()
-    clock.tick(60)
+mainGame().run()
